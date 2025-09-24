80848e264c25f7c1c6efcde86766627f2f627515
Dimitrios Pantazis <dpanta94@gmail.com>
2025-09-24 21:25:59 +0300

    Fix constructor overwrite

--- tests/_support/Helper/Schema/MockModelSchemaWithRelationship.php
+++ tests/_support/Helper/Schema/MockModelSchemaWithRelationship.php
@@ -7,12 +7,11 @@ use StellarWP\Schema\Tables\Contracts\Table_Interface;
 use StellarWP\Models\ValueObjects\Relationship;
 
 class MockModelSchemaWithRelationship extends SchemaModel {
-	public function __construct( array $attributes = [] ) {
-		parent::__construct( $attributes );
-
+	protected function constructRelationships(): void {
 		$this->defineRelationship( 'posts', Relationship::MANY_TO_MANY, MockRelationshipTable::class );
 		$this->defineRelationshipColumns( 'posts', 'mock_model_id', 'post_id' );
 	}
+
 	public function getTableInterface(): Table_Interface {
 		return new MockRelationshipModelTable();
 	}
