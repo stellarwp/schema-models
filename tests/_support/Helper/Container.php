c839624e32ebbcfb887d68a70bc4dd7c0366104c
Dimitrios Pantazis <dpanta94@gmail.com>
2025-09-24 20:44:09 +0300

    Fix tests

---
+++ tests/_support/Helper/Container.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Schema Models's container for tests.
+ *
+ * @since TBD
+ *
+ * @package StellarWP\SchemaModels\Tests
+ */
+
+declare( strict_types=1 );
+
+namespace StellarWP\SchemaModels\Tests;
+
+use StellarWP\ContainerContract\ContainerInterface;
+
+use lucatume\DI52\Container as DI52_Container;
+
+/**
+ * Schema Models's container for tests.
+ *
+ * @since TBD
+ *
+ * @package StellarWP\SchemaModels\Tests
+ */
+class Container extends DI52_Container implements ContainerInterface {}
---
+++ tests/_support/Helper/Schema/MockModelSchema.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace StellarWP\SchemaModels\Tests\Schema;
+
+use StellarWP\SchemaModels\SchemaModel;
+use StellarWP\Schema\Tables\Contracts\Table_Interface;
+
+class MockModelSchema extends SchemaModel {
+	public function getTableInterface(): Table_Interface {
+		return new MockModelTable();
+	}
+}
